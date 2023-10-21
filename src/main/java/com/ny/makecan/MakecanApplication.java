b5a6a29defe8fbd4820b569a681a3f4f41e4d077
nyjeong <ddoubledb@gmail.com>
2023-10-21 17:28:52 +0900

    initial commit

---
+++ src/main/java/com/ny/makecan/MakecanApplication.java
@@ -0,0 +1,13 @@
+package com.ny.makecan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakecanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakecanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ny/makecan/MakecanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ny.makecan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakecanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
